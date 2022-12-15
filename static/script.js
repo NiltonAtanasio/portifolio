713ed85a659a381649cc712b0d4ba4b64a36cf6b
Nilton Atanazzio <nii.cesar@hotmail.com>
2022-12-15 18:00:54 -0300

    🚀 Section habilidades

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+/* Abre e fecha aside mobile */
 const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
 
@@ -6,4 +7,16 @@ menuMobile.addEventListener('click', () => {
     ? menuMobile.classList.replace('bi-list', 'bi-x')
     : menuMobile.classList.replace('bi-x', 'bi-list')
   body.classList.toggle('menu-nav-active')
+})
+
+/* Fecha menu quando clicar em algum item e muda o icone para list */
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+  item.addEventListener("click", () => {
+    if(body.classList.contains("menu-nav-active")){
+      body.classList.remove("menu-nav-active")
+      menuMobile.classList.replace("bi-x", "bi-list")
+    }
+  })
 })
