0378e6ff3e17a83962ed0716e0f435b4c596e0f2
Nilton Atanazzio <nii.cesar@hotmail.com>
2022-12-21 20:54:17 -0300

    🍃 data-anime

--- static/script.js
+++ static/script.js
@@ -1,22 +1,42 @@
 /* Abre e fecha aside mobile */
-const menuMobile = document.querySelector('.menu-mobile')
-const body = document.querySelector('body')
+const menuMobile = document.querySelector(".menu-mobile")
+const body = document.querySelector("body")
 
-menuMobile.addEventListener('click', () => {
-  menuMobile.classList.contains('bi-list')
-    ? menuMobile.classList.replace('bi-list', 'bi-x')
-    : menuMobile.classList.replace('bi-x', 'bi-list')
-  body.classList.toggle('menu-nav-active')
+menuMobile.addEventListener("click", () => {
+  menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")
+    : menuMobile.classList.replace("bi-x", "bi-list")
+  body.classList.toggle("menu-nav-active")
 })
 
 /* Fecha menu quando clicar em algum item e muda o icone para list */
-const navItem = document.querySelectorAll('.nav-item')
+const navItem = document.querySelectorAll(".nav-item")
 
-navItem.forEach(item => {
+navItem.forEach((item) => {
   item.addEventListener("click", () => {
     if(body.classList.contains("menu-nav-active")){
       body.classList.remove("menu-nav-active")
       menuMobile.classList.replace("bi-x", "bi-list")
     }
   })
+})
+
+// atributo data-anime
+
+const item = document.querySelectorAll("[data-anime]")
+
+const animeScroll = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85
+
+  item.forEach((element) => {
+    if(windowTop > element.offsetTop) {
+      element.classList.add("animate")
+    } else {
+      element.classList.remove("animate")
+    }
+  })
+}
+
+window.addEventListener("scroll", () => {
+  animeScroll()
 })
